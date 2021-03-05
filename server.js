7715313f39400369d6eb513e3b447a51f7d32ce3
jarell Boone <jarellboone4@gmail.com>
2021-03-05 00:35:49 -0500

    Finished the javascript for adding and viewing the employees

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const connection = mysql.createConnection({
   database: 'employee_db',
 });
 
+connection.connect((err) => {
+  if (err) throw err;
+  mainQuestion();
+})
+
 const mainQuestion = () => {
 
   inquirer.prompt([
@@ -16,13 +21,13 @@ const mainQuestion = () => {
       type: 'list',
       name: 'allfunctions',
       message: 'What would you like do?',
-      choices: ['add departments', 'add roles, add employees', 'view departments', 'view roles', 'view employees', 'update employee roles']
+      choices: ['add departments', 'add roles', 'add employees', 'view departments', 'view roles', 'view employees', 'update employee roles']
     }
 
   ])
     .then(function (Answer) {
       switch (Answer.allfunctions) {
-        case 'add department':
+        case 'add departments':
           addDepartment()
           break
 
@@ -47,7 +52,7 @@ const mainQuestion = () => {
           break
 
         case 'update employee roles':
-          viewEmployeeRoles()
+          updateEmployeeRoles()
           break
 
 
@@ -55,27 +60,195 @@ const mainQuestion = () => {
           break;
       }
     }
-
     )
-  const addDepartment = () => {
+}
+
+
+const addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departments',
+      message: 'What department would you like to add?'
+
+    }
+  ])
+    .then((answer) => {
+
+      connection.query(
+        'INSERT INTO departments SET ?',
+        {
+          name: answer.departments
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("You have successfully added an department")
+          mainQuestion()
+        })
+
+
+    })
+
+  const addRoles = () => {
     inquirer.prompt([
       {
         type: 'input',
-        name: 'departments',
-        message: 'What department would you like to add?',
+        name: 'role',
+        message: 'What role would you like to add?'
 
       }
-        .then((answer) => {
-          const query = 'SELECT Finance FROM department WHERE ?';
-          connection.query(query, {}, (err, res) => {
+    ])
+      .then((answer) => {
+
+        connection.query(
+          'INSERT INTO role SET ?',
+          {
+            name: answer.role
+          },
+          (err) => {
             if (err) throw err;
-            res.forEach(({ department }) => {
+            console.log("You have successfully added a role")
+            mainQuestion()
+          })
 
 
-            });
-          };
 
+
+
+
+
+
+      })
+  }
+
+  const addEmployees = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'employee',
+        message: 'What employee would you like to add?'
+
+      }
+    ])
+      .then((answer) => {
+
+        connection.query(
+          'INSERT INTO employee SET ?',
+          {
+            name: answer.employee
+          },
+          (err) => {
+            if (err) throw err;
+            console.log("You have successfully added an employee")
+            mainQuestion()
+          })
+
+          const viewEmployees = (x) => {
+            console.log(x)
+            connection.query(`SELECT * FROM ${x}`,
+              (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                mainQuestion();
+              }
+            )
+          }
+
+
+
+
+
+
+      })
+  }
+
+
+  const viewDepartments = (b) => {
+    console.log(x)
+    connection.query(`SELECT * FROM ${b}`,
+      (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainQuestion();
+      }
+    )
+  }
+
+
+
+  const viewRoles = (b) => {
+    console.log(x)
+    connection.query(`SELECT * FROM ${b}`,
+      (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainQuestion();
+      }
+    )
+  }
+
+  function updateRole() {
+    const updating = "SELECT id, first_name, last_name, role_id  FROM employee";
+    connection.query(updating, function (err, res) {
+      if (err) throw err;
+      console.table(res); // using table to show list of employee's
+
+      let allRoles = 'SELECT * FROM employee_role';
+      let roles = connection.query(allRoles, (err, data) => {
+        let roleChoices = [];
+
+        for (let i = 0; i < data.length; i++) {
+          roleChoices.push({
+            name: data[i].title,
+            value: data[i].id
+          })
         }
 
-}
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Select the employee by ID that you would like to update",
+            name: "employee",
 
+          },
+
+          {
+
+            type: "list",
+            message: "Select which role you'd like to update the employee to",
+            name: "newRole",
+           choices: roleChoices
+          }
+
+        ])
+          .then((answer) => {
+            connection.query(
+              'UPDATE employee SET role_id = ? WHERE id = ? ', 
+
+              [
+                answer.newRole,
+                answer.employee
+
+              ],
+
+              (err) => {
+                if (err) throw err;
+                console.log("You have successfully udpated a role")
+                // ask initial question again.
+                mainQuestion();
+              }
+            );
+          });
+
+
+      })
+    });
+  };
+
+
+
+
+
+
+
+}
